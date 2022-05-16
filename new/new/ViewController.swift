d05fa2f2973a9d3ad0b31d0ca9623a5e6bf87eb1
owenfok <owenfok119@gmail.com>
2022-05-16 21:25:25 +0800

    new

---
+++ new/new/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  new
+//
+//  Created by Fok, Owen Wai Yin on 16/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //NSLog("Hello")
+    }
+
+
+}
+
